a0ab26e990cb13f0e91a0e7b076af89b02843273
Naina Paliwal <nainapaliwal8989@gmail.com>
2024-10-17 18:44:14 +0530

    added comment to serializer.py

--- courses/serializers.py
+++ courses/serializers.py
@@ -1,11 +1,4 @@
-# from rest_framework import serializers
-# from . models import Course
-
-
-# class CourseSerializer(serializers.HyperlinkedModelSerializer):
-# 	class Meta:
-# 		model = Course
-# 		fields = ('id','url', 'name','language','price')
+#this is our serializer page
 
 
 from rest_framework import serializers
